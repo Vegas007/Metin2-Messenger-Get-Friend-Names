8bb9e81c175f074a55e1e205b7fe9878546ff59a
vegas <vegas_project1337@yahoo.com>
2018-08-30 23:42:16 +0300

    Initial commit

---
+++ Python Version/smth.py
@@ -0,0 +1,13 @@
+def GetFriendNames(self):
+	import uiMessenger
+	friendNamesList = []
+
+	if self.interface:
+		if self.interface.wndMessenger:
+			for group in self.interface.wndMessenger.groupList[uiMessenger.FRIEND]:
+				memberList = group.GetFriendNames()[:]
+				if memberList:
+					for member in memberList:
+						friendNamesList.append(member.GetName())
+
+	return tuple(friendNamesList)
