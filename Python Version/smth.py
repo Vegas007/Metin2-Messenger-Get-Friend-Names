07254e6cc5b6edf3d47396edf7f541cb5ab3f3a4
Vegas007 <vegas_project1337@yahoo.com>
2019-04-10 01:32:05 +0300

    Update smth.py

--- Python Version/smth.py
+++ Python Version/smth.py
@@ -5,9 +5,7 @@ def GetFriendNames(self):
 	if self.interface:
 		if self.interface.wndMessenger:
 			for group in self.interface.wndMessenger.groupList[uiMessenger.FRIEND]:
-				memberList = group.GetMemberList()[:]
-				if memberList:
-					for member in memberList:
-						friendNamesList.append(member.GetName())
+				for member in group.GetMemberList()[:]:
+					friendNamesList.append(member.GetName())
 
 	return tuple(friendNamesList)
